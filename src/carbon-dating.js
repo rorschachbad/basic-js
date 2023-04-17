2c9db5058b84be54bd646b9c7a8d491dcf658556
rorschachbad <72745338+rorschachbad@users.noreply.github.com>
2023-04-18 01:47:50 +0300

    feat: complete tranform array task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-function dateSample(sampleActivity) {
-  
+function dateSample(/* sampleActivity */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  const resArr = [...arr];
+  let discardNext = '--discard-next';
+  let discardPrev = '--discard-prev';
+  let doubleNext = '--double-next';
+  let doublePrev = '--double-prev';
+
+  for (let i = 0; i < resArr.length; i++) {
+    if (Array.isArray(resArr)) {
+      if (resArr[i] === discardNext) {
+        resArr.splice(i, 2);
+      } else if (resArr[i + 1] === discardPrev && resArr[i + 1] !== undefined) {
+        resArr.splice(i, 2)
+      } else if (resArr[i] === doubleNext) {
+        resArr.splice(i, 1, resArr[i + 1]);
+      } else if (resArr[i] === doublePrev && resArr[i - 1] !== undefined) {
+        resArr.splice(i, 1, resArr[i - 1])
+      }
+    } else {
+      return Error("'arr' parameter must be an instance of the Array!")
+    }
+  }
+  return resArr;
 }
+
+transform([1, 2, 3, '--double-prev', 4, 5])
 
 module.exports = {
   transform
